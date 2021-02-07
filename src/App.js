2237ee4d24dcbd18d5390bc15051373ce1642134
Nick James <nick@twic.ai>
2021-02-07 12:33:14 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import '@google/model-viewer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <model-viewer
+        style={{
+          width: "100%",
+          height: "800px",
+          top: "0px", 
+          backgroundColor: "grey"
+        }}
+        src="my-bag.glb"
+        camera-controls
+        shadow-intensity="1">
+      </model-viewer>
+      <h2>Trademark info</h2>
+      <p>
+        5. What are some additional examples of fair use?
+        In general, the following uses are considered fair use:
+
+        Use in comparative advertising that is an opinion (or a truthful fact). Example:
+        Statement that “BRAND X tastes better than BRAND Z.”
+        Use to advertise goods that are being sold or repaired or for which a product is suitable for use. Examples:
+        Statement by a repair shop: “We repair ROLLS-ROYCE cars.”
+        Use of “iPhone” in non-stylized form on packaging for phone cases to indicate that it is usable with an iPhone.
+        Below are additional examples from cases in select jurisdictions.
+      </p>
+      <a
+        rel="noreferrer"
+        target="_blank"
+        href='https://www.inta.org/fact-sheets/fair-use-of-trademarks-intended-for-a-non-legal-audience/'>
+          International trademark association
+      </a>
+    </>
   );
 }
 
